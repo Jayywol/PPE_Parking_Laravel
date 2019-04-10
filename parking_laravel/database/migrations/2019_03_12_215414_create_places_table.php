e945c820058509ca52765ae46a7f860f7760e351
Alex <42928961+Jayywol@users.noreply.github.com>
2019-04-10 21:53:10 +0200

    Add "lib" -> database

--- parking_laravel/database/migrations/2019_03_12_215414_create_places_table.php
+++ parking_laravel/database/migrations/2019_03_12_215414_create_places_table.php
@@ -15,6 +15,7 @@ class CreatePlacesTable extends Migration
     {
         Schema::create('places', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('lib');
             $table->boolean('available')->default(TRUE);
             $table->timestamps();
         });
--- parking_laravel/database/seeds/PlacesTableSeeder.php
+++ parking_laravel/database/seeds/PlacesTableSeeder.php
@@ -15,24 +15,28 @@ class PlacesTableSeeder extends Seeder
           array(
               array(
                   'id' => 1,
+                  'lib' => "Alpha",
                   'available' => FALSE,
                   'created_at' => '2019-01-01 00:00:00',
                   'updated_at' => '2019-01-01 00:00:01',
               ),
               array(
                   'id' => 2,
+                  'lib' => "Beta",
                   'available' => FALSE,
                   'created_at' => '2019-01-02 08:00:00',
                   'updated_at' => '2019-01-02 08:00:01',
               ),
               array(
                   'id' => 3,
+                  'lib' => "Charlie",
                   'available' => TRUE,
                   'created_at' => '2019-01-03 12:00:00',
                   'updated_at' => '2019-01-03 12:00:01',
               ),
               array(
                   'id' => 4,
+                  'lib' => "Delta",
                   'available' => TRUE,
                   'created_at' => '2019-01-04 16:00:00',
                   'updated_at' => '2019-01-04 16:00:01',
